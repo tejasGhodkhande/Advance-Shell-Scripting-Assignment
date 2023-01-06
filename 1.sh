f858b756fb4d955e15ea159a5db7f4f8f997b1a2
Tejas Jivan Ghodkhande <121927286+tejasGhodkhande@users.noreply.github.com>
2023-01-06 15:48:48 +0530

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+{
+	echo record1 word2
+	echo record2 word4
+	echo record3 word6
+} | awk '
+	BEGIN { c=0 }
+	{ c++ }
+	{ print c ": first rule" }
+	/4$/ { exit 42; print c " " $1 }
+	{ print c ": "  $2 }
+	END { print "quitting ..." }
+'
+echo "return code: $?"
