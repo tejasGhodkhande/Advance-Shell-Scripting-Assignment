fd920d6ee1e4f4b58c8d0b6d36a0f6f4dc89d3dd
Tejas Jivan Ghodkhande <121927286+tejasGhodkhande@users.noreply.github.com>
2023-01-08 19:04:48 +0530

    Create 7.sh

---
+++ 7.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+C=0
+[() {	# overwrite the [ builtin
+	
+	# Increase and print a counter
+	((C++))
+	echo $C
+
+	# this gives an infinite loop:
+	if [ $C -gt 100 ] ; then
+		echo "never printed"
+		exit 1
+	fi
+}
+
+if [ "$VAR" ]; then
+	echo "VAR is not empty"	 #never reached
+fi
+
