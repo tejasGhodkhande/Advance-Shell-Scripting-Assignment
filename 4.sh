560963c94e9ab39d1c0eb4cf4e43f0c1e6e96c90
Tejas Jivan Ghodkhande <121927286+tejasGhodkhande@users.noreply.github.com>
2023-01-08 19:03:12 +0530

    Create 4.sh

---
+++ 4.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+awk '
+	# change the record separator to anything
+	# which is not an alphanumeric (we consider
+	# a different word to start at each alphnum-
+	# eric character)
+	BEGIN { RS="[^[:alnum:]]+" }
+	# now each word is a separate record
+
+	$0 == prev { print prev; ret=1; next }
+	{ prev = $0 }	
+	END { exit ret }
+' input.txt
